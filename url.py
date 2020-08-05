4aad5b47a7002c0a89c7774072503a63b72669ba
unknown <geshen@ESG.360ES.CN>
2020-08-05 22:31:01 +0800

    space

--- url.py
+++ url.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 import urllib,re,requests,sys,datetime
-logo="""
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+indomain=open("in.txt")
+print logo="""
  _   _ ____  _        ____ ____      _    ____
 | | | |  _ \| |      / ___|  _ \    / \  | __ )
 | | | | |_) | |     | |  _| |_) |  / _ \ |  _ |
 | |_| |  _ <| |___  | |_| |  _ <  / ___ \| |_) |
  \___/|_| \_\_____|  \____|_| \_\/_/   \_\____/
 """
-print logo
-oknum=nonum=wrong=find1=find2=unknow=0
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
-indomain=open("in.txt")
+oknum = 0
+nonum = 0
+wrong = 0
+find1 = 0
+find2 = 0
+unknow = 0
 incount=len(open('in.txt','rU').readlines())
 path_list = []
 domain_list = []
@@ -23,57 +27,56 @@ for line in indomain:
     line=line.strip('\n')
     try:
         r = requests.get(line,verify=True,timeout=2,headers=headers)
-        oknum=oknum+1
-        print '('+str(oknum)+"/"+str(incount)+')'+' '+line+' ok'
+        oknum = oknum + 1
+        print '('+str(oknum) + "/" + str(incount) + ')' + ' ' + line + ' ok'
     except requests.RequestException as e:
-        #oknum=oknum+1
-        nonum=nonum+1
-        print '('+str(oknum)+"/"+str(incount)+')'+' '+line+' cannot read';
+        nonum = nonum + 1
+        print '(' + str(oknum) + "/" +str(incount) + ')' + ' ' + line + ' cannot read'
     else:
         data = r.text
-        link_list =re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')" ,data)
+        link_list = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')" ,data)
         for url in link_list:
             proto, rest = urllib.splittype(url)
             res, rest = urllib.splithost(rest)
             if res == '':
-                wrong=wrong+1
+                wrong = wrong+1
             #elif not rest:print rest
             else:
                 if res == None:
-                    wrong=wrong+1
+                    wrong = wrong+1
                 elif str(proto) == 'http':
-                    fulldomain= str(proto)+'://'+ res
-                    fullpath=str(proto)+'://'+ res + rest
+                    fulldomain = str(proto) + '://' + res
+                    fullpath = str(proto)+'://'+ res + rest
                     domain_list.append(fulldomain)
                     path_list.append(fullpath)
-                    find1=find1+1
+                    find1 = find1 + 1
                 elif str(proto) == 'https':
-                    fulldomain= str(proto)+'://' + res
-                    fullpath=str(proto)+'://'+ res + rest
+                    fulldomain = str(proto) + '://' + res
+                    fullpath = str(proto) + '://' + res + rest
                     domain_list.append(fulldomain)
                     path_list.append(fullpath)
-                    find2=find2+1
-                else:unknow=unknow+1
+                    find2 = find2 + 1
+                else: unknow = unknow + 1
 indomain.close()
 output_domain = open("ok/fulldomain.txt", "w+")
-domain_list_t=set(domain_list)
+domain_list_t = set(domain_list)
 for m in domain_list_t:
     output_domain.write(str(m+'\n'))
 output_domain.close()
 
-path_list_t=set(path_list)
+path_list_t = set(path_list)
 output_path = open("ok/fullpath.txt", "w+")
 for n in path_list_t:
     output_path.write(str(n+'\n'))
 output_path.close()
 print '--'*40
-print 'success '+str(oknum)+' domain'
-print 'lose '+str(nonum)+' domains'
-print 'find http '+str(find1)
-print 'find https '+str(find2)
-print 'find urls '+str(find1+find2)
-print 'find distinct domain '+str(len(open('ok/fulldomain.txt','rU').readlines()))+' distinct path '+str(len(open('ok/fullpath.txt','rU').readlines()))
-print 'bad url '+str(wrong)+' unknow wrong '+str(unknow)
+print 'success ' + str(oknum) + ' domain'
+print 'lose ' + str(nonum) + ' domains'
+print 'find http ' + str(find1)
+print 'find https ' + str(find2)
+print 'find urls ' + str(find1+find2)
+print 'find distinct domain ' + str(len(open('ok/fulldomain.txt','rU').readlines())) + ' distinct path ' + str(len(open('ok/fullpath.txt','rU').readlines()))
+print 'bad url '+ str(wrong) + ' unknow wrong ' + str(unknow)
 end = datetime.datetime.now()
-print 'use time '+str(end-start)
+print 'use time ' + str(end-start)
 print '--'*40
